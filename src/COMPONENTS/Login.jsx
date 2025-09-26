0fbc7d1d79511f8c420eee8c1e625c0fb3210ed4
KONJETI GNANA VENKATA NAGA GOPI PRASANTH <2300031235@kluniversity.in>
2025-09-26 08:40:30 +0530

    Update Login.jsx

--- src/COMPONENTS/Login.jsx
+++ src/COMPONENTS/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://portb-production.up.railway.app/api/auth/login", {
+      const response = await fetch("https://portb-production.up.railway.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
